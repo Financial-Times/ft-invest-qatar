c7a4884f45247dd8e4a933e331385073a779cc68
Niv Suresh <nivsuresh06@gmail.com>
2022-04-05 11:12:49 +0100

    Moving main out of App

--- pages/_app.js
+++ pages/_app.js
@@ -4,30 +4,28 @@ import Layout from '../components/Layout';
 import ReactGA from 'react-ga';
 
 function MyApp({ Component, pageProps, router }) {
-  ReactGA.initialize('UA-93924635-35');
+	ReactGA.initialize('UA-93924635-35');
 
-  useEffect(() => {
-    TagManager.initialize({ gtmId: 'GTM-T66ZTL5' });
-  }, []);
+	useEffect(() => {
+		TagManager.initialize({ gtmId: 'GTM-T66ZTL5' });
+	}, []);
 
-  useEffect(() => {
-    ReactGA.pageview(document.location.href);
-    router.events.on('routeChangeComplete', () => {
-      ReactGA.pageview(document.location.href);
-    });
+	useEffect(() => {
+		ReactGA.pageview(document.location.href);
+		router.events.on('routeChangeComplete', () => {
+			ReactGA.pageview(document.location.href);
+		});
 
-    return () => {
-      router.events.off('routeChangeComplete', ReactGA.pageview(router.asPath));
-    };
-  }, [router]);
+		return () => {
+			router.events.off('routeChangeComplete', ReactGA.pageview(router.asPath));
+		};
+	}, [router]);
 
-  return (
-    <Layout>
-      <main className="main" id="content">
-        <Component {...pageProps} key={router.route} />
-      </main>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Component {...pageProps} key={router.route} />
+		</Layout>
+	);
 }
 
 export default MyApp;
